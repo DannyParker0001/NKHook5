0a368e9cdcf4ef794aa43369b215228624ced789
DisabledMallis <mallisiscool@gmail.com>
2019-10-25 22:18:11 -0400

    hotfix

--- API/Events/MouseUpEvent.cs
+++ API/Events/MouseUpEvent.cs
@@ -25,12 +25,6 @@ namespace NKHook5.API.Events
                 {
                     try
                     {
-                        int count = 0;
-                        foreach (Bloon b in Game.getBTD5().getBloons())
-                        {
-                            Logger.Log("Bloon: " + count + " Type: " + b.getType());
-                            count++;
-                        }
                         Event.Invoke(this, new EventArgs());
                     }
                     catch (NullReferenceException) { }
